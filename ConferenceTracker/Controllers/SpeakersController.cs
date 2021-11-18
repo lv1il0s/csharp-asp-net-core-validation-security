ed2a3b4af6c23181a9e55b1bcb3416987aed9f79
Milosevic Milos <Milos.Milosevic@zumtobelgroup.com>
2021-11-18 22:52:26 +0100

    New - "Add Antiforgery and Binding to the Create Action"
    Setup the HttpPost Create action to validate an AntiForgeryToken and use Bind on our speaker parameter to prevent stuffing.
    
    On our HttpPost Create action, add the ValidateAntiForgeryToken attribute. That's it! Anytime you make a Form in ASP.NET Core it, automatically adds a hidden input with the antiforgery token.
    Instead of just accepting speaker as is, we should use the Bind attribute with an argument of "Id,FirstName,LastName,Description,EmailAddress,PhoneNumber" to restrict the action to only accepting those properties. Otherwise someone could maliciously alter their submission to set the IsStaff property.

--- ConferenceTracker/Controllers/SpeakersController.cs
+++ ConferenceTracker/Controllers/SpeakersController.cs
@@ -46,7 +46,8 @@ namespace ConferenceTracker.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Administrators")]
-        public IActionResult Create(Speaker speaker)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Speaker speaker)
         {
             if (ModelState.IsValid)
             {
